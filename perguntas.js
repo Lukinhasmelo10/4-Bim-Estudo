f52c610356ed5e2c5182514fb2869d3a5101ae96
Lukinhasmelo10 <0000109830116xsp@al.educacao.sp.gov.br>
2024-11-08 11:35:02 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,23 @@
 criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'Call Of Duty: Black Ops 2',
+    'Qual a temática do jogo da franquia COD?',
+    'Guerra Fria renovada, futuro próximo e ciber-guerra.'
 )
 
 criaCartao(
-    'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'Need For Speed: Underground 2',
+    'Qual o carro inicial do game?',
+    'O carro inicail é um Nissan  350z, tunado com mais de 280 cavalos, um bela máquina nostalgica'
 )
 
 criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'Brawl Stars',
+    'Qual o melhor mod de Brawl?',
+    'O melhor modo e mas vantajoso é  fute brawl'
 )
 
 criaCartao(
-    'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'Rocket League',
+    'Quais as patentes do game?',
+    'O ranking de Rocket League é constituído de sete níveis: Bronze, Prata, Ouro, Platina, Diamante, Campeão e Grande Campeão.'
 )
